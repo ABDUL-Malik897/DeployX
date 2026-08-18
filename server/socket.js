dea819be58276745a5c6e51f5df4d38b69c11ff1
ABDUL-Malik897 <malikabdulxd@gmail.com>
2026-08-18 08:30:28 +0530

    Configure production deployment

--- server/socket.js
+++ server/socket.js
@@ -6,7 +6,7 @@ const initializeSocket = (server) => {
 
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:3001",
+            origin: ["http://localhost:3001","https://deploy-x-ashen.vercel.app"],
             credentials: true
         }
     });
