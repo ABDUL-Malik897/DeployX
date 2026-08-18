28165dadcb3f9da642e5a476152c4a4527b158f6
ABDUL-Malik897 <malikabdulxd@gmail.com>
2026-08-18 07:08:41 +0530

    fixing

--- server/utils/github.js
+++ server/utils/github.js
@@ -1,9 +1,28 @@
+// const fs = require("fs");
+// const path = require("path");
+// const { App } = require("@octokit/app");
+
+// const privateKeyPath = path.resolve(process.env.GITHUB_PRIVATE_KEY_PATH);
+// const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+// const githubApp = new App({
+//     appId: process.env.GITHUB_APP_ID,
+//     privateKey
+// });
+
+// module.exports = githubApp;
+
 const fs = require("fs");
 const path = require("path");
 const { App } = require("@octokit/app");
 
-const privateKeyPath = path.resolve(process.env.GITHUB_PRIVATE_KEY_PATH);
-const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+let privateKey;
+if (process.env.GITHUB_PRIVATE_KEY) {
+    privateKey = process.env.GITHUB_PRIVATE_KEY.replace(/\\n/g, "\n");
+} else {
+    const privateKeyPath = path.resolve(process.env.GITHUB_PRIVATE_KEY_PATH);
+    privateKey = fs.readFileSync(privateKeyPath, "utf8");
+}
+
 const githubApp = new App({
     appId: process.env.GITHUB_APP_ID,
     privateKey
