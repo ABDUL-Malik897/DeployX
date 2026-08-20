c5ccce08fc6d971743270ba3f1113a4b75f36f43
ABDUL-Malik897 <malikabdulxd@gmail.com>
2026-08-20 06:35:23 +0530

    fixing Connection

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_API_URL || "http://localhost:4000");
+const socket = io(
+    process.env.REACT_APP_API_URL || "http://localhost:4000",
+    {
+        transports: ["websocket"]
+    }
+);
 
 export default socket;
